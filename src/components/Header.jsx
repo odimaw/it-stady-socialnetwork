fae40cc6bb8d6e2c8c24323f25470025f972d3e8
odim <odimaw@yandex.ru>
2020-06-21 20:16:15 +0300

    socialnetwork 01

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-260nw-1029171697.jpg'></img>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://images.wallpaperscraft.ru/image/pliazh_palma_okean_127914_300x168.jpg'></img>
+        </div>
+        <div>
+            ava + description
+    </div>
+        <div>
+            My posts
+      <div>
+                New posts
+      </div>
+            <div>
+                post 1
+      </div>
+            <div>
+                post 2
+      </div>
+        </div>
+    </div>
+}
+
+export default Profile;
