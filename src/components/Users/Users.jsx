08c7c3a24f33fcea87a274bd65fb823266750f27
odimaw <odimaw@yandex.ru>
2020-07-22 06:37:56 +0300

    51 lesson AXIOS

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,41 +1,54 @@
 import React from 'react';
 import styles from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png';
 
 let Users = (props) => {
 
+    let getUsers = () => {
+
+    
     if (props.users.length === 0) {
-    props.setUsers([
-        { id: 1, photoUrl: 'https://24smi.org/public/media/celebrity/2015/09/21/1442831684-dmitrij-nagiev.jpg', followed: false, fullName: 'Dmitry', status: 'I am a boss', location: { city: 'Voronech', country: 'Russia' } },
-        { id: 2, photoUrl: 'https://24smi.org/public/media/celebrity/2015/09/21/1442831684-dmitrij-nagiev.jpg', followed: true, fullName: 'Sasha', status: 'I am a boss too', location: { city: 'Moscow', country: 'Russia' } },
-        { id: 3, photoUrl: 'https://24smi.org/public/media/celebrity/2015/09/21/1442831684-dmitrij-nagiev.jpg', followed: false, fullName: 'Andrew', status: 'I am a boss too', location: { city: 'Kiev', country: 'Ukraine' } },
-        { id: 4, photoUrl: 'https://24smi.org/public/media/celebrity/2015/09/21/1442831684-dmitrij-nagiev.jpg', followed: false, fullName: 'Sveta', status: 'I am a boss too', location: { city: 'Minsk', country: 'Belarussia' } },
-    ]) }
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
-                <span>
-                    <div>
-                        <img src={u.photoUrl} className={styles.userPhoto} />
-                    </div>
-                    <div>
-                        {u.followed ? <button onClick={() => { props.unfollow(u.id) }} >Unfollow</button>
-                            : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+        
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+              
+                props.setUsers(response.data.items);
+            });
 
-                    </div>
-                </span>
-                <span>
+        // props.setUsers([
+        //     { id: 1, photoUrl: 'https://24smi.org/public/media/celebrity/2015/09/21/1442831684-dmitrij-nagiev.jpg', followed: false, fullName: 'Dmitry', status: 'I am a boss', location: { city: 'Voronech', country: 'Russia' } },
+        //     { id: 2, photoUrl: 'https://24smi.org/public/media/celebrity/2015/09/21/1442831684-dmitrij-nagiev.jpg', followed: true, fullName: 'Sasha', status: 'I am a boss too', location: { city: 'Moscow', country: 'Russia' } },
+        //     { id: 3, photoUrl: 'https://24smi.org/public/media/celebrity/2015/09/21/1442831684-dmitrij-nagiev.jpg', followed: false, fullName: 'Andrew', status: 'I am a boss too', location: { city: 'Kiev', country: 'Ukraine' } },
+        //     { id: 4, photoUrl: 'https://24smi.org/public/media/celebrity/2015/09/21/1442831684-dmitrij-nagiev.jpg', followed: false, fullName: 'Sveta', status: 'I am a boss too', location: { city: 'Minsk', country: 'Belarussia' } },
+        // ]) 
+    } }
+        return <div>
+            <button onClick={getUsers}>Get Users</button>
+            {
+                props.users.map(u => <div key={u.id}>
                     <span>
-                        <div>{u.fullName}</div>
-                        <div>{u.status}</div>
+                        <div>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
+                        </div>
+                        <div>
+                            {u.followed ? <button onClick={() => { props.unfollow(u.id) }} >Unfollow</button>
+                                : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+
+                        </div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <span>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </span>
+                        <span>
+                            <div>{'u.location.country'}</div>
+                            <div>{'u.location.city'}</div>
+                        </span>
                     </span>
-                </span>
-            </div>)
-        }
-    </div>
-}
+                </div>)
+            }
+        </div>
+    }
 
-export default Users;
+    export default Users;
