5e1db9df4560cf9409388aed4e3b47d9023de12b
odimaw <odimaw@yandex.ru>
2020-08-03 03:01:15 +0300

    61 withCredentials

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import { Route, BrowserRouter } from "react-router-dom";
@@ -9,23 +8,18 @@ import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
 
 
 const App = () => {
   return (
       <div className='app-wrapper'>
-        <Header />
+        <HeaderContainer />
         <Navbar />
         <div className='app-wrapper-content'>
           <Route path='/dialogs' 
           render={ () => <DialogsContainer 
-            // store={props.store} 
-
-            // state = {props.state}
-            // dialogsPage = {props.state.dialogsPage} 
-            // dispatch = {props.dispatch}
-            // addMessage = {props.addMessage}
-            // updateNewMessageText = {props.updateNewMessageText}
+           
             />} />
           <Route path='/profile/:userId?'  
           render={ () => <ProfileContainer 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import s from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
     return <header className={s.header}>
         <img src='https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-260nw-1029171697.jpg'></img>
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login
+            : <NavLink to={'/login'}>Login</NavLink> }
+        </div>
     </header>
 }
 
