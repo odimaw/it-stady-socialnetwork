a0c6ca3dbfbc04681add176cb2ba34daa261057d
odimaw <odimaw@yandex.ru>
2020-10-08 05:53:41 +0300

    94 React.Suspense React.lazy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
 import { Route, withRouter } from "react-router-dom";
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import UsersContainer from './components/Users/UsersContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import LoginPage from './components/Login/login';
 import { connect } from 'react-redux';
 import { initializeApp } from './redux/app-reducer';
 import Preloader from './components/common/Preloader/Preloader';
 import { compose } from 'redux';
-
+// import ProfileContainer from './components/Profile/ProfileContainer';
+// import DialogsContainer from './components/Dialogs/DialogsContainer';
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class App extends Component {
   componentDidMount() {
@@ -22,22 +23,24 @@ class App extends Component {
   }
   render() {
     if (!this.props.initialized) {
-    return <Preloader />
+      return <Preloader />
     }
     return (
       <div className='app-wrapper'>
         <HeaderContainer />
         <Navbar />
         <div className='app-wrapper-content'>
-          <Route path='/dialogs'
-            render={() => <DialogsContainer />} />
-          <Route path='/profile/:userId?'
-            render={() => <ProfileContainer />} />
-          <Route path='/news' render={() => <News />} />
-          <Route path='/music' render={() => <Music />} />
-          <Route path='/settings' render={() => <Settings />} />
-          <Route path='/users' render={() => <UsersContainer />} />
-          <Route path='/login' render={() => <LoginPage />} />
+          <React.Suspense fallback={<Preloader />}>
+            <Route path='/dialogs'
+              render={() => <DialogsContainer />} />
+            <Route path='/profile/:userId?'
+              render={() => <ProfileContainer />} />
+            <Route path='/news' render={() => <News />} />
+            <Route path='/music' render={() => <Music />} />
+            <Route path='/settings' render={() => <Settings />} />
+            <Route path='/users' render={() => <UsersContainer />} />
+            <Route path='/login' render={() => <LoginPage />} />
+          </React.Suspense>
         </div>
       </div>
     )
@@ -50,4 +53,4 @@ const mapStateToProps = (state) => ({
 
 export default compose(
   withRouter,
-   connect(mapStateToProps, {initializeApp})) (App);
+  connect(mapStateToProps, { initializeApp }))(App);
