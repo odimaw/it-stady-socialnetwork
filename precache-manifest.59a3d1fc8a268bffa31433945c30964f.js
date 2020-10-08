6650b8e4b8d846713c3db7b2af01932cc07ac55a
odimaw <odimaw@yandex.ru>
2020-10-08 06:54:53 +0300

    Updates

---
+++ precache-manifest.59a3d1fc8a268bffa31433945c30964f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2393bdbc6181e7d31528bed42415b25f",
+    "url": "/it-stady-socialnetwork/index.html"
+  },
+  {
+    "revision": "729faf594072a535e2df",
+    "url": "/it-stady-socialnetwork/static/css/3.2a4b83bc.chunk.css"
+  },
+  {
+    "revision": "b83e9eb661bb92bd6b55",
+    "url": "/it-stady-socialnetwork/static/css/4.b0fab986.chunk.css"
+  },
+  {
+    "revision": "f07a0775176268e604aa",
+    "url": "/it-stady-socialnetwork/static/css/main.631d42b8.chunk.css"
+  },
+  {
+    "revision": "b7759f3800029b53532a",
+    "url": "/it-stady-socialnetwork/static/js/2.7effb207.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/it-stady-socialnetwork/static/js/2.7effb207.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "729faf594072a535e2df",
+    "url": "/it-stady-socialnetwork/static/js/3.c6df41bb.chunk.js"
+  },
+  {
+    "revision": "b83e9eb661bb92bd6b55",
+    "url": "/it-stady-socialnetwork/static/js/4.e697c90c.chunk.js"
+  },
+  {
+    "revision": "f07a0775176268e604aa",
+    "url": "/it-stady-socialnetwork/static/js/main.822bfbc1.chunk.js"
+  },
+  {
+    "revision": "085f0986b6cfdf8f3a49",
+    "url": "/it-stady-socialnetwork/static/js/runtime-main.5de6526d.js"
+  },
+  {
+    "revision": "a61850fd1c43a03fde2e0fd960d4226b",
+    "url": "/it-stady-socialnetwork/static/media/preloader.a61850fd.gif"
+  }
+]);
